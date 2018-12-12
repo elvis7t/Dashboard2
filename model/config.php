36e3d43d107b4bc9e59da300207bde9cec311550
elvis7t <elvis7t@gmail.com>
2018-12-12 13:49:15 -0200

    Commit Inicial

---
+++ model/config.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('America/Sao_paulo');
+define('DB_HOSTNAME','localhost');
+define('DB_USERNAME','id7329975_root');
+define('DB_PASSWORD','102030');
+define('DB_DATABASE','id7329975_infra'); 
+define('DB_PREFIX',null);
+define('DB_CHARSET','utf8');
+?>
+
+
